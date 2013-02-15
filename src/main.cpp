198d1a0f4348744cf5926919da9f6aafa079d378
Rob Ashton <robashton@codeofrob.com>
2013-02-15 22:15:47 +0200

    reading only a few bytes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <leveldb/db.h>
 #include <iostream>
+#include <sstream>
 
-#define SIZE 1024 * 1024 * 50
+#define SIZE 1024 * 1024 * 10
 
 int main() {
   leveldb::DB* db;
@@ -15,21 +16,40 @@ int main() {
 
   char* arbitraryData = new char[SIZE];
   for(int x = 0; x < SIZE ; x++) 
-    arbitraryData[x] = 32;
+    arbitraryData[x] = 'A';
+
+  clock_t startTime = clock();
 
   leveldb::Slice slicedData(arbitraryData, SIZE);
 
-  status = db->Put(write_options, "key", arbitraryData);
+  std::stringstream str;
+  str << "key";
+  std::string key = str.str();
+
+  status = db->Put(write_options, key, arbitraryData);
+  std::cout << key << std::endl;
 
   std::cout << "Slice is " <<  slicedData.size() << std::endl;
   std::cout << "Wrote to database, status: " << status.ok() << std::endl;
+  
+  std::cout << "About to do some iteration yo'" << std::endl;
 
-  std::string outputValue;
-  status = db->Get(leveldb::ReadOptions(), "key", &outputValue);
+  leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+  for (it->Seek(key); it->Valid() && it->key().ToString() <= key; it->Next()) {
+    leveldb::Slice slice = it->value();
 
-  leveldb::Slice outputSlice(outputValue);
-  std::cout << "Read from database, status: " << status.ok() << " bytes: " << outputSlice.size() << std::endl;
+    std::stringstream st;
+    st << (char)slice.data()[0];
+    st << (char)slice.data()[1];
+    st << (char)slice.data()[2];
+    st << (char)slice.data()[3];
 
+    std::cout << "The first four bytes are " << st.str() << std::endl;
+  }
+
+  std::cout << double( clock() - startTime ) / (double)CLOCKS_PER_SEC<< " seconds." << std::endl;
+
+  delete[] arbitraryData;
   delete db;
   std::cout << "Closed database" << std::endl;
 }
